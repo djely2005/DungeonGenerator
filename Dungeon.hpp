dd90f4f09ae54460842d89069084fb78d40af76a
djely2005 <elyass162005@gmail.com>
2026-04-17 09:44:27 +0100

    Making Dungeon a Singleton

--- Dungeon.hpp
+++ Dungeon.hpp
@@ -6,8 +6,16 @@
 class Dungeon{
 private:
     std::vector<std::vector<Tile*>> grid;
+    static Dungeon* instance;
+private:
+    Dungeon(){}
 public:
     void generate(int, int);
     void render();
     void findPath();
+    static Dungeon* getInstance(){
+        if(instance == nullptr)
+            instance = new Dungeon();
+        return instance;
+    }
 };
